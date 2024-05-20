4bfd9ba47505b8ff9997b75a99c78e2befa3ac36
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-20 15:18:46 +0200

    Added: header on home.blade.php and 3 link to go on other pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,25 @@
-<h1>Hello World</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <header>
+        <ul>
+            <li><a href="{{url('/page1')}}">Page 1</a></li>
+            <li><a href="{{url('/page2')}}">Page 2</a></li>
+            <li><a href="{{url('/page3')}}">Page 3</a></li>
+        </ul>
+    </header>
+
+    <main>
+        <h1>Hello World</h1>
+    </main>
+    
+</body>
+</html>
+
+
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,14 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/header', function () {
-    return view('header');
+Route::get('/page1', function () {
+    return view('page1');
+});
+
+Route::get('/page2', function () {
+    return view('page2');
+});
+
+Route::get('/page3', function () {
+    return view('page3');
 });
